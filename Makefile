a4adea027ee1b28d4dbfba628f2e3c56022f9130
pnor <pnoreggio@gmail.com>
2025-07-20 22:56:44 -0700

    Add flags

--- Makefile
+++ Makefile
@@ -14,32 +14,32 @@ MAGICK_FLAGS=`Magick++-config --cppflags --cxxflags --ldflags --libs`
 FLAGS=-std=c++23 -Wall -Wextra -Werror=switch -Wfatal-errors
 DEBUG_FLAGS=-ggdb -O0 -fsanitize=address
 RELEASE_FLAGS=-O3 -march=native -fno-trapping-math -fno-math-errno -flto -fomit-frame-pointer
+INCLUDE_DIR=./include
 
 SOURCES=main.cpp
 OUTPUT=ttypic
+TEST_IMAGE=./test_image.jpg
 
-dev: build
-	echo "Done ~"
+dev: debug
+	echo "Done"
 
 debug: build
-	./${OUTPUT}
+	./${OUTPUT} ${TEST_IMAGE}
 
 fast: build-fast
-	./${OUTPUT}
+	./${OUTPUT} ${TEST_IMAGE}
 
 check:
 	${CLANG_TIDY} ${SOURCES}
 
 build: 
-	${BEAR} -- ${COMPILER} ${SOURCES} \
-	${FLAGS} ${DEBUG_FLAGS} ${LINKER_FLAGS} ${MAGICK_FLAGS} -o ${OUTPUT} \
-	&& ./${OUTPUT}
+	${BEAR} -- ${COMPILER} -I${INCLUDE_DIR} ${SOURCES} \
+	${FLAGS} ${DEBUG_FLAGS} ${LINKER_FLAGS} ${MAGICK_FLAGS} -o ${OUTPUT}
 
 build-fast: fast
-	${COMPILER} ${SOURCES} \
+	${COMPILER} -I${INCLUDE_DIR} ${SOURCES} \
 	-O3 \
-	${FLAGS} ${RELEASE_FLAGS} ${LINKER_FLAGS} ${MAGICK_FLAGS} -o ${OUTPUT} \
-	&& ./${OUTPUT}
+	${FLAGS} ${RELEASE_FLAGS} ${LINKER_FLAGS} ${MAGICK_FLAGS} -o ${OUTPUT}
 
 clean:
 	rm ${OUTPUT}
--- main.cpp
+++ main.cpp
@@ -1,135 +1,173 @@
 #include <Magick++.h>
+#include <cstdio>
+#include <filesystem>
+#include <format>
 #include <iostream>
 #include <optional>
-#include <filesystem>
-#include <utility>
-#include <format>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "include/argparse.hpp"
 
 namespace {
 
-using UInt = uint16_t;
-using Decimal = float;
+  using UInt = uint16_t;
+  using Decimal = float;
 
-struct Dimensions {
-  UInt width;
-  UInt height;
-};
+  struct Dimensions {
+    UInt width;
+    UInt height;
+  };
 
 /**
  * width:height of a character
  */
-constexpr Decimal CHARACTER_ASPECT_RATIO = 0.5;
+  constexpr Decimal CHARACTER_ASPECT_RATIO = 0.5;
+  constexpr Decimal DEFAULT_TERMINAL_SCALE_FACTOR = 0.5;
+  constexpr UInt DEFAULT_WIDTH = 160;
+  constexpr UInt DEFAULT_HEIGHT = 22;
 
 // ===================== Command Line Parsing =====================
 
-void helpMessage(){std::cout << "usage: ttypic [image-path]\n"; }
+  void helpMessage() { std::cout << "usage: ttypic [image-path]\n"; }
 
-// NOLINTBEGIN
-// Don't lint the pointer arithmetic involved with argv
-
-std::optional<std::filesystem::path> getImageFromCmdlineArgs(const int argc,
-                                                             char *argv[]) {
-  if (argc <= 1) {
-    return std::nullopt;
-  }
-  return argv[1] != nullptr ? std::make_optional(std::filesystem::path(
-                                  std::string_view(argv[1])))
-                            : std::nullopt;
-}
-
-void handleHelpFlagIfPresent(const int argc, char *argv[]) {
-  if (argc <= 1 || argv[1] == nullptr) {
-    return;
+  std::filesystem::path
+  getImageFromCmdlineArgs(const argparse::ArgumentParser &program) {
+    const auto &path = program.get<std::string>("image_path");
+    return std::filesystem::path(path);
   }
 
-  const std::string_view firstArg = argv[1];
-
-  if (firstArg == "-h" || firstArg == "--help") {
-      helpMessage();
-      exit(0);
+  Dimensions getTerminalSize() {
+    int cols = DEFAULT_WIDTH;
+    int lines = DEFAULT_HEIGHT;
+#ifdef TIOCGSIZE
+    struct ttysize termSize{};
+    ioctl(STDIN_FILENO, TIOCGSIZE, &termSize);
+    cols = termSize.termSize_cols;
+    lines = termSize.termSize_lines;
+#elif defined(TIOCGWINSZ)
+    struct winsize termSize{};
+    // NOLINTNEXTLINE
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &termSize);
+    cols = termSize.ws_col;
+    lines = termSize.ws_row;
+#endif /* TIOCGSIZE */
+    return {.width = static_cast<UInt>(cols), .height = static_cast<UInt>(lines)};
   }
-}
-// NOLINTEND
+
+  Dimensions getDimensionsFromCmdline(const argparse::ArgumentParser &program) {
+    const std::optional<UInt> width = program.present<int>("--width");
+    const std::optional<UInt> height = program.present<int>("--height");
+
+    if (width.has_value() && height.has_value()) {
+      return Dimensions{.width = width.value(), .height = height.value()};
+    }
+
+    Dimensions terminalDimensions = getTerminalSize();
+    terminalDimensions.width =
+      static_cast<UInt>(static_cast<Decimal>(terminalDimensions.width) *
+                        DEFAULT_TERMINAL_SCALE_FACTOR);
+    terminalDimensions.height =
+      static_cast<UInt>(static_cast<Decimal>(terminalDimensions.height) *
+                        DEFAULT_TERMINAL_SCALE_FACTOR);
+    return terminalDimensions;
+  }
 
 // ===== Image Processing ========================
 
-Dimensions determineImageSize(const Magick::Geometry &imageSize,
-                              const UInt maxWidth, const UInt maxHeight) {
-  const Decimal scaleX = static_cast<Decimal>(maxWidth) /
-                         static_cast<Decimal>(imageSize.width());
-  const Decimal scaleY =
+  Dimensions determineImageSize(const Magick::Geometry &imageSize,
+                                const UInt maxWidth, const UInt maxHeight) {
+    const Decimal scaleX =
+      static_cast<Decimal>(maxWidth) / static_cast<Decimal>(imageSize.width());
+    const Decimal scaleY =
       static_cast<Decimal>(maxHeight) /
       (static_cast<Decimal>(imageSize.height()) * CHARACTER_ASPECT_RATIO);
 
-  const Decimal scale = std::min(scaleX, scaleY);
-  std::cout << "maxWidth = " << maxWidth << "\n";
-  std::cout << "maxHeight = " << maxHeight << "\n";
-  std::cout << "scale x: " << scaleX << "\n";
-  std::cout << "scale y: " << scaleY << "\n";
-  std::cout << "scale : " << scale << "\n";
-  std::cout << "image width : " << imageSize.width() << "\n";
-  std::cout << "image height : " << imageSize.height() << "\n";
-  std::cout << "---" << "\n";
-  return {
-      .width =
-          static_cast<UInt>(static_cast<Decimal>(imageSize.width()) * scale),
-      .height = static_cast<UInt>(static_cast<Decimal>(imageSize.height()) *
-                                  scale * CHARACTER_ASPECT_RATIO),
+    const Decimal scale = std::min(scaleX, scaleY);
+    return {
+    .width =
+    static_cast<UInt>(static_cast<Decimal>(imageSize.width()) * scale),
+    .height = static_cast<UInt>(static_cast<Decimal>(imageSize.height()) *
+                                scale * CHARACTER_ASPECT_RATIO),
   };
-}
+  }
 
-void printImageArt(const std::filesystem::path &imagePath, const UInt termWidth,
-                   const UInt termHeight) {
-  const Magick::Image image(imagePath.c_str());
-  const Dimensions targetDim =
+  Magick::Image getImage(const std::filesystem::path &imagePath) {
+    try {
+      const Magick::Image image(imagePath.c_str());
+      return image;
+    } catch (const std::exception& _) {
+      std::cout << imagePath.c_str() << " is not a valid image!" << "\n";
+      exit(1);
+    }
+  }
+
+  void printImageArt(const std::filesystem::path &imagePath, const UInt termWidth,
+                     const UInt termHeight, const char glyph) {
+    const Magick::Image image = getImage(imagePath);
+    const Dimensions targetDim =
       determineImageSize(image.size(), termWidth, termHeight);
-  // const Dimensions targetDim = { .width = termWidth, .height = termHeight };
 
-  std::cout << "targetDim width " << targetDim.width << "\n";
-  std::cout << "targetDim height " << targetDim.height << "\n";
-
-  for (UInt j = 0; j < targetDim.height; j++) {
-    for (UInt i = 0; i < targetDim.width; i++) {
-      const Decimal percentX =
+    for (UInt j = 0; j < targetDim.height; j++) {
+      for (UInt i = 0; i < targetDim.width; i++) {
+        const Decimal percentX =
           static_cast<Decimal>(i) / static_cast<Decimal>(termWidth);
-      const Decimal percentY =
+        const Decimal percentY =
           static_cast<Decimal>(j) / static_cast<Decimal>(termHeight);
 
-      const auto imgX =
-          static_cast<UInt>(percentX * static_cast<Decimal>(image.size().width()));
-      const auto imgY =
-          static_cast<UInt>(percentY * static_cast<Decimal>(image.size().height()));
+        const auto imgX = static_cast<UInt>(
+          percentX * static_cast<Decimal>(image.size().width()));
+        const auto imgY = static_cast<UInt>(
+          percentY * static_cast<Decimal>(image.size().height()));
 
-      const Magick::Color color = image.pixelColor(imgX, imgY);
-      constexpr double MAGICK_QUANTUM_RANGE = 65535.0;
-      const auto red =
+        const Magick::Color color = image.pixelColor(imgX, imgY);
+        constexpr double MAGICK_QUANTUM_RANGE = 65535.0;
+        const auto red =
           static_cast<uint8_t>(color.quantumRed() * 255 / MAGICK_QUANTUM_RANGE);
-      const auto green = static_cast<uint8_t>(color.quantumGreen() * 255 /
-                                              MAGICK_QUANTUM_RANGE);
-      const auto blue = static_cast<uint8_t>(color.quantumBlue() * 255 /
-                                             MAGICK_QUANTUM_RANGE);
+        const auto green = static_cast<uint8_t>(color.quantumGreen() * 255 /
+                                                MAGICK_QUANTUM_RANGE);
+        const auto blue = static_cast<uint8_t>(color.quantumBlue() * 255 /
+                                               MAGICK_QUANTUM_RANGE);
 
-      // Good Glyphs
-      // - █
-      // - 8
-      // - #
-      constexpr std::string_view GLYPH = "#";
-      const std::string colorText =
-          std::format("\x1b[38;2;{};{};{}m{}\x1b[0m", red, green, blue, GLYPH);
+        const std::string colorText =
+          std::format("\x1b[38;2;{};{};{}m{}\x1b[0m", red, green, blue, glyph);
 
-      std::cout << colorText;
+        std::cout << colorText;
+      }
+      std::cout << "\n";
     }
-    std::cout << "\n";
   }
-}
 
 } // namespace
 
 auto main(int argc, char *argv[]) -> int {
-  handleHelpFlagIfPresent(argc, argv);
+  argparse::ArgumentParser program("ttypic");
+  program.add_description("Print a picture as ASCII art");
+
+  program.add_argument("image_path").help("Path of image to display");
+
+  program.add_argument("-w", "--width")
+    .help("Width in characters of output image")
+    .scan<'i', int>();
+
+  program.add_argument("-h", "--height")
+    .help("Height in characters of output image")
+    .scan<'i', int>();
+
+  program.add_argument("-g", "--glyph")
+    .help("Character to use to draw ascii art")
+    .default_value("#");
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << "\n\n";
+    std::cerr << program;
+    return 1;
+  }
 
   const std::optional<std::filesystem::path> content =
-      getImageFromCmdlineArgs(argc, argv);
+    getImageFromCmdlineArgs(program);
 
   if (!content.has_value()) {
     helpMessage();
@@ -142,20 +180,15 @@ auto main(int argc, char *argv[]) -> int {
     exit(0);
   }
 
-  // TODO
-  // - cmdline args:
-  //   + width and height
-  //   + default should infer from terminal size
-  //   + glyph to use for image
-  // - graceful handle non images
-  // - size config
-
   try {
     Magick::InitializeMagick(*argv);
-
-    constexpr UInt WIDTH = 160 / 2 ;
-    constexpr UInt HEIGHT = 45 / 2 ;
-    printImageArt(path, WIDTH, HEIGHT);
+    const Dimensions artSize = getDimensionsFromCmdline(program);
+    const std::string glyph = program.get<std::string>("--glyph");
+    if (glyph.size() <= 0) {
+      std::cout << "Glyph must be a single non-empty character!" << "\n";
+      exit(1);
+    }
+    printImageArt(path, artSize.width, artSize.height, glyph[0]);
   } catch (std::exception &e) {
     std::cout << "Ran into exception! " << e.what() << "\n";
   }
